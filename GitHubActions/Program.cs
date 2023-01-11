78bd8c81e7b8d5cc9ce045606a5e3591783a520d
danilo.colombi <daniloc.t@hotmail.com>
2023-01-11 13:32:02 -0300

    Allow all origins

--- GitHubActions/Program.cs
+++ GitHubActions/Program.cs
@@ -1,3 +1,5 @@
+using GitHubActions.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Total", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 
 var app = builder.Build();
 
